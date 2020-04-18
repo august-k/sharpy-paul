ac258cec759508ee802cf58935cac3f3900b190d
august-k <august.kaplan@gmail.com>
2020-04-18 18:59:37 -0400

    reverted overlord building

--- sharpy-sc2-develop/paul_plans/overlord_scout_plan.py
+++ sharpy-sc2-develop/paul_plans/overlord_scout_plan.py
@@ -10,33 +10,19 @@ class OverlordScoutOrder(BuildOrder):
 
     def __init__(self):
         scout_pattern = SequentialList(
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(4 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(6 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(8 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(10 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(12 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(14 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(16 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(18 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(20 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(22 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(24 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(26 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(28 * 60)),
-            Step(None, ActUnitOnce(UnitTypeId.OVERLORD)),
             Step(None, OverlordScoutMain(), skip_until=RequiredTime(30 * 60)),
         )
 
